baf523869e909c124264891bc10075001f0478af
Christopher Nguyen <Chris_ngu@pm.me>
2020-07-07 12:38:37 -0500

    fetching json + randomized quotes

--- Random Quote Machine/src/App.js
+++ Random Quote Machine/src/App.js
@@ -1,16 +1,20 @@
+// Work on retweet URI component redirect
+
 import React, { useEffect } from 'react';
+import { Redirect } from 'react-router-dom';
 
 export default class QuoteMachine extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      quote: '',
-      author: ''
+      quote: 'DEFAULT QUOTE',
+      author: 'DEFAULT AUTHOR'
     }
 
     this.freeCodeCampChecker = this.freeCodeCampChecker.bind(this);
     this.generateQuote = this.generateQuote.bind(this);
+    this.retweet = this.retweet.bind(this);
   }
 
   componentDidMount = () => {
@@ -28,21 +32,30 @@ export default class QuoteMachine extends React.Component {
   }
 
   generateQuote = () => {
-    
-    this.setState({
-      quote: 'new Quote',
-      author: 'new Author'
-    });
+    // Pulls Information for http response, converts to usable json file, then finally usuable index of json
+
+    fetch('https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json')
+      .then((data) => data.json())
+      .then((data) => {
+        let selectedQuote = data.quotes[Math.floor(Math.random() * data.quotes.length)];
+
+        this.setState({
+          quote: selectedQuote.quote,
+          author: selectedQuote.author
+        });
+      })
+  }
+  retweet = () => {
+    let link = "https://twitter.com/intent/tweet?text=" + encodeURIComponent('"' + this.state.quote + '" ' + this.state.author);
   }
 
   render() {
     return (
-
       <div id="quote-box">
-        <h1 id="text">"SAMPLE QUOTE HERE"</h1>
-        <p id="author">-AUTHOR_HERE</p>
+        <h1 id="text">{this.state.quote}</h1>
+        <p id="author">{this.state.author}</p>
         <button id="new-quote" onClick={this.generateQuote} />
-        <a id='tweet-quote' href="twitter.com/intent/tweet"></a>
+        <a id='tweet-quote' onClick={this.retweet}>TWEET ICON HERE</a>
       </div>
     );
   }
